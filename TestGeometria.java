a019c6746b630a17f4179293977bfbb0d856a382
Joaquin <[joaquin.diaz.ramirez.8897@gmail.com]>
2017-11-02 13:31:26 +0100

    TestGeometria.java

--- TestGeometria.java
+++ TestGeometria.java
@@ -1,31 +1,22 @@
+/**Generamos documentacion para el archivopubli
+test geometria
+@author Joaquin Diaz
+@version 1.0
+*/
 public class TestGeometria{
 	public static void main (String [] args){
 		
-		int lado = 5;
-		int areaCuadrado = lado * lado;
-		
-	    int baseTriangulo = 6;
-		int alturaTriangulo = 10;
-		int areaTriangulo = (baseTriangulo * alturaTriangulo)/2;
-		
-		int baseRectangulo = 8;
-		int alturaRectangulo = 4;
-		int areaRectangulo = baseRectangulo * alturaRectangulo;
-		
-		double radio = 5;
-		double areaCirculo = radio *  radio * 3.14;
-		
-    int resultadoCuadrado = Geometria.areaCuadrado (areaCuadrado);
-	System.out.println(" El area del cuadrado es: " + areaCuadrado);
+    int resultadoCuadrado = Geometria.calculaAreaCuadrado (5);
+	System.out.println(" El area del cuadrado es: " + resultadoCuadrado);
 	
-	int resultadoTriangulo = Geometria.areaTriangulo (areaTriangulo);
-	System.out.println(" El area del triangulo es: " + areaTriangulo);
+	int resultadoTriangulo = Geometria.calculaAreaTriangulo (6,10);
+	System.out.println(" El area del triangulo es: " + resultadoTriangulo);
 	
-	int resultadoRectangulo = Geometria.areaRectangulo (areaRectangulo);
-	System.out.println(" El area del rectangulo es: " + areaRectangulo);
+	int resultadoRectangulo = Geometria.calculaAreaRectangulo (8,4);
+	System.out.println(" El area del rectangulo es: " + resultadoRectangulo);
 	
-	double resultadoCirculo = Geometria.areaCirculo (areaCirculo);
-	System.out.println(" El area del circulo es: " + areaCirculo);
+	double resultadoCirculo = Geometria.calculaAreaCirculo (5);
+	System.out.println(" El area del circulo es: " + resultadoCirculo);
 	}
 }
 		
